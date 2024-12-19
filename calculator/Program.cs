838bed9ff1f0bce31690ec5a514b0e91311e1ef1
shincy jerin <shincy jerin@LAPTOP-V7GIHEKQ>
2024-12-19 11:03:22 +0530

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Animal
+{
+    public string Name;
+    public void display()
+    {
+        Console.WriteLine("Im an Animal");
+    }
+}
+class Dog : Animal
+{
+    public void getName()
+    {
+        Console.WriteLine("My name is " + Name);
+    }
+}
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Name = "test";
+        dog.display();
+        dog.getName();
+
+    }
+}
